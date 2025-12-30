5fd58df4e434232e65b0a538bf1c09b2d8e172d0
Tim Liu <2743095620@qq.com>
2025-12-30 11:59:30 -0500

    use double type to replace float

---
+++ compound.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+double compound(int amount, double rate, int years)
+{
+    double current = 0;
+    double compound = 0;
+
+    for (int i = 0; i < years; ++i)
+    {
+        /*The current amount at the start of a year is the new deposit
+	 * amount plus compound amount from last year.
+	 */
+    current = compound + amount;
+
+    /*The amount amount is just the current amount (at the start of the year) plus interest.
+     */
+    compound = current * (1 + rate);
+    }
+    return compound;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
+        printf("Please input parameters, like this:\n");
+	printf("%s <deposit_amount> <interest_rate> <years>\n" , argv[0]);
+	printf("%s 100 0.04 10\n" , argv[0]);
+	return 1;
+    }
+
+    int amount = atoi(argv[1]);
+    double rate = atof(argv[2]);// interest rate is a 'double' type, not 'int'
+    int years = atoi(argv[3]);
+
+    printf("your input are:%d %f %d\n" , amount, rate, years );
+
+    double total = compound(amount, rate, years);
+    printf("Totally you will get: $%f\n", total);
+
+    return 0;
+}
