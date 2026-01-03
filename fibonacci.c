c1ee9ee9f13e026124a1d27afbf0fe54b79fd7aa
Tim Liu <2743095620@qq.com>
2026-01-03 10:50:30 -0500

    add ascii.c and fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+int fibonacci(int n)
+{
+    if (n == 0) { return 0;}
+
+    int memory_size = (n + 1) * 4;
+    int *arr = (int *) malloc(memory_size);
+
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i <= n; ++i)
+    {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    
+    int s = arr[n];
+
+    //release momory
+    free(arr);
+    arr = NULL;
+
+    return s;
+}
+
+int main(int argc, char *argv[])
+{
+    while(true)
+    {   
+        printf("WHich Fibonacci number do you want to know?\n");
+        printf("Please input ->:");
+
+        int n;
+    
+        scanf("%d" , &n);
+
+        printf ("Your input is %d\n" , n);
+
+        if (n < 0)
+        {
+            printf("Please input an integer >= 0.\n");
+	    return 1;
+        }
+
+        int f = fibonacci(n);
+
+        printf ("The %d-th Fibonacci number is: %d\n" , n, f);
+
+        printf("Do you want to continue? Y/N\n");
+
+        char c;
+
+        scanf(" %c",&c);
+    
+        if (c == 'Y')
+        {
+            printf("Good! Let's continue!\n");
+        }
+        else if(c =='N')
+        {
+            printf("Good bye!\n");
+	    break;
+        }
+	else
+	{
+	    printf("Incorrect input, bye\n");
+	    break;
+	}   
+   
+    }
+
+    printf("Fibonacci ended.\n");
+    return 0;
+}
+    
+        
