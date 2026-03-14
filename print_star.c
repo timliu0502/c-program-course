66d2f74911cd4c5765044ed6da26674ed9549ed0
Tim Liu <2743095620@qq.com>
2026-03-14 12:17:51 -0400

    print diamond

--- print_star.c
+++ print_star.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int print_space(int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        printf(" ");
+    }
+    return 0;
+}
+
+int print_char(int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+	printf("*");
+    }
+    return 0;
+}
+
 int print_triangle(int row)
 {
     for (int i = 1; i<= row; ++i)
@@ -37,6 +55,18 @@ int main(int argc, char*argv[])
     int a = atoi(argv[1]);
 
     print_triangle(a);
+
+    int row = a - 1;
+
+    for (int i = 0; i < row; ++i)
+    {
+        print_space(i + 1);
+        
+
+	int star_num = (2 * row - 1 - 2 * i);
+	print_char(star_num);
+	printf("\n");
+    }
 
     return 0;
 
