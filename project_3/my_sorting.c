92e024603dbabf95d4d0b3fe2e10bbadb708e03c
timliu0502 <timmml310105@gmail.com>
2026-09-19 12:19:19 -0400

    passing by reference

---
+++ project_3/my_sorting.c
@@ -0,0 +1,9 @@
+#include"my_sorting.h"
+
+void swap(int *x, int *y)
+{
+	int t = *x;
+	*x = *y;
+	*y = t;
+}
+		
---
+++ project_3/my_sorting.h
@@ -0,0 +1,7 @@
+#ifndef _MY_SORTING_H_
+#define _MY_SORTING_H_
+
+void swap(int *x, int *y);
+int find_min(int *arr, int start, int len);
+
+#endif
